f0d57f2256e9c30a6403b5f2130d0ff6e280e71e
BachHoang <bachbop9999@gmail.com>
2021-02-03 11:08:09 +0700

    upload file

--- eCert/Controllers/HomeController.cs
+++ eCert/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System;
 
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,6 +25,33 @@ namespace eCert.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public void UploadFile(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    //string storePath = "C:\\Users\\BachHV\\Desktop\\CertificateFile";
+                    string folderPath = Server.MapPath("~/UploadedFiles");
+                    if (!Directory.Exists(folderPath))
+                    {
+                        Directory.CreateDirectory(folderPath);
+                    }
+                    string _FileName = Path.GetFileName(file.FileName);
+                    //string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    string _path = Path.Combine(folderPath, _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "File Uploaded Successfully!!";
+            }
+            catch
+            {
+                ViewBag.Message = "File upload failed!!";
+            }
+        }
+
 
         public ActionResult About()
         {
