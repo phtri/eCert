d3c1e3f6625a429f3ef5b61a54bc6669776bd710
HaPhucTri <HaPhucTri>
2021-02-03 11:04:48 +0700

    Certificate DAO

---
+++ eCert/Daos/CertificateDAO.cs
@@ -0,0 +1,28 @@
+﻿using eCert.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eCert.Daos
+{
+    public class CertificateDAO
+    {
+        private readonly DataProvider _dataProvider;
+        
+        public CertificateDAO()
+        {
+            _dataProvider = new DataProvider();
+        }
+
+        /**
+         * Add, update, delete
+         * Example: dataProvinder.ADD("INSERT INTO Person VALUES( @param1 , @param2 )", new object[] { tbName, tbAge });
+         */
+        public void CreateCertificate(Certificate c)
+        {
+            _dataProvider.ADD_UPDATE_DELETE("INSERT INTO Certificates VALUES (@param1, @param2,@param3, @param4, @param5, @param5, @param6, @param7, @param8, @param9, @param10, @param11, @param12", new object[] { c.CertificateName, c.VerifyCode, c.FileName, c.Type, c.Format, c.Description, c.Content, c.Hashing, c.UserId, c.OrganizationId, c.created_at, c.updated_at });
+        }
+
+    }
+}
--- eCert/Daos/DataProvider.cs
+++ eCert/Daos/DataProvider.cs
@@ -14,6 +14,11 @@ namespace eCert.Daos
         string connStr = WebConfigurationManager.ConnectionStrings["Database"].ConnectionString;
 
 
+        /**
+         * Get list object
+         * Example: DataTable dataTable = dataProvider.GET_LIST_OBJECT( "select * from Ships where name like @param1", new object[] { "%abc%" });
+         */
+
         public DataTable GET_LIST_OBJECT(string query, object[] parameter)
         {
             SqlConnection con = null;
@@ -50,6 +55,45 @@ namespace eCert.Daos
                 con.Close();
             }
         }
+
+        /**
+         * Add, update, delete
+         * Example: dataProvinder.ADD("INSERT INTO Person VALUES( @param1 , @param2 )", new object[] { tbName, tbAge });
+         */
+        public void ADD_UPDATE_DELETE(string query, object[] parameter)
+        {
+            SqlConnection con = null;
+            SqlCommand cmd = null;
+            try
+            {
+                con = new SqlConnection(connStr);
+                con.Open();
+                cmd = new SqlCommand(query, con);
+                if (parameter != null)
+                {
+                    string[] listParam = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listParam)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            cmd.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
 
 
     }
