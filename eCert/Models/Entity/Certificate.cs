5bec9d2e03901757d6392c300f1cf6d70176ce8c
HaPhucTri <HaPhucTri>
2021-02-08 16:03:05 +0700

    ViewModel

--- eCert/Models/Entity/Certificate.cs
+++ eCert/Models/Entity/Certificate.cs
@@ -2,26 +2,23 @@
 using System.ComponentModel.DataAnnotations;
 using System.Web;
 
-namespace eCert.Models
+namespace eCert.Models.Entity
 {
     public class Certificate
     {
         public int CertificateID { get; set; }
-        [Required(ErrorMessage = "Name is Required. It cannot be empty")]
         public string CertificateName { get; set; } = "";
         public string VerifyCode { get; set; } = "";
-        public string FileName { get; set; } = "";
-        public string Type { get; set; } = "";
+        public string Issuer { get; set; } = "";
         public string Format { get; set; } = "";
         public string Description { get; set; } = "";
-        public string Content { get; set; } = "";
         public string Hashing { get; set; } = "";
+        public int ViewCount { get; set; } = 0;
         public DateTime created_at { get; set; }
         public DateTime updated_at { get; set; }
         //Foreign key
         public int UserId { get; set; }
         public int OrganizationId { get; set; }
-        // not database entity
-        public HttpPostedFileBase CertificateFile { get; set; }
+        
     }
 }
