16b078c9ff43b4fd6211b00b183c289b03bef3bf
HaPhucTri <HaPhucTri>
2021-03-01 18:04:43 +0700

    Fap Demo Web service

---
+++ Demo_FAP_WebService/Demo_FAP_WebService/UsersService.asmx.cs
@@ -0,0 +1,38 @@
+﻿using Demo_FAP_WebService.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Demo_FAP_WebService
+{
+    /// <summary>
+    /// Summary description for UsersService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class UsersService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public List<User> GetStudentList()
+        {
+            return new List<User>()
+            {
+                new User(){FirstName = "Pham", MiddleName = "Thanh", LastName = "Ha", Gender = true, DOB = new DateTime(1999, 9, 1), PhoneNumber = "0912345678", PersonalEmail = "ha@mail.com", AcademicEmail = "hafap@fpt.edu.vn", RollNumber = "HE130576"},
+                new User(){FirstName = "Pham", MiddleName = "Thanh", LastName = "Ha", Gender = true, DOB = new DateTime(1999, 9, 1), PhoneNumber = "0912345678", PersonalEmail = "ha@mail.com", AcademicEmail = "hafap@fpt.edu.vn", RollNumber = "HE130576"},
+                new User(){FirstName = "Pham", MiddleName = "Thanh", LastName = "Ha", Gender = true, DOB = new DateTime(1999, 9, 1), PhoneNumber = "0912345678", PersonalEmail = "ha@mail.com", AcademicEmail = "hafap@fpt.edu.vn", RollNumber = "HE130576"}
+            };
+        } 
+    }
+}
