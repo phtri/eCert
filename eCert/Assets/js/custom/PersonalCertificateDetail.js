5439420010817bb57d97313b9631187ea14338ef
BachHoang <bachbop9999@gmail.com>
2021-03-08 14:44:24 +0700

    add confirm dialog

---
+++ eCert/Assets/js/custom/PersonalCertificateDetail.js
@@ -0,0 +1,22 @@
+﻿function handleDeleteAccount(title, msg, id) {
+    $('#confirmModal').modal('show');
+    $('#confirmTitle').html(title);
+    $('.modal-body').html(msg);
+    $('#confirmModal').on('click', '.btn-yes', function (e) {
+        $.ajax({
+            type: "POST",
+            url: '/Certificate/Delete',
+            context: document.body,
+            data: { certId: id },
+            //contentType: 'application/json; charset=utf-8',
+            success: function (result) {
+               
+            },
+            error: function (req, err) {
+                //debugger;  
+                console.log(err);
+                alert("Error has occurred..");
+            }
+        });
+    });
+}
