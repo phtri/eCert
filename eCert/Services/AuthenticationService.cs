bdf4f1aaefdec561a7facc4be4cf048e818d6805
BachHoang <bachbop9999@gmail.com>
2021-03-09 12:14:48 +0700

    doing login function

---
+++ eCert/Services/AuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.Owin.Security.Cookies;
+using System.Security.Claims;
+using Microsoft.Owin.Security;
+
+
+namespace eCert.Services
+{
+    public class AuthenticationService
+    {
+       
+    }
+}
