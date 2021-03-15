20ed8cde0f5c375785ffb6f88b2a36cba19aa4f5
Nguyen Minh Tuan <sangotunhien197@gmail.com>
2021-03-15 20:54:16 +0700

    fix font certificate

--- eCert/Controllers/CertificateController.cs
+++ eCert/Controllers/CertificateController.cs
@@ -270,5 +270,10 @@ namespace eCert.Controllers
                 return sw.GetStringBuilder().ToString();
             }
         }
+
+        public ActionResult C()
+        {
+            return View("~/Views/Shared/Certificate.cshtml");
+        }
     }
 }
