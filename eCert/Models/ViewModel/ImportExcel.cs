6c9a61093678df61f6d289ca8aef251e0b3adecb
BachHoang <bachbop9999@gmail.com>
2021-04-01 20:31:49 +0700

    fix upload cert, diploma

--- eCert/Models/ViewModel/ImportExcel.cs
+++ eCert/Models/ViewModel/ImportExcel.cs
@@ -8,12 +8,14 @@ namespace eCert.Models.ViewModel
 {
     public class ImportExcel
     {
-        [Required(ErrorMessage = "Please select file")]
-        [FileExt(Allow = ".xls,.xlsx", ErrorMessage = "Only excel file")]
+        [Required(ErrorMessage = "Please select File")]
+        [FileExt(Allow = ".xls,.xlsx", ErrorMessage = "Only excel File")]
         public HttpPostedFileBase File { get; set; }
         [CampusValidation(ErrorMessage = "Please select Campus")]
         public int CampusId { get; set; }
         [CampusValidation(ErrorMessage = "Please select Signature")]
         public int SignatureId { get; set; }
+        [CampusValidation(ErrorMessage = "Please select Education System")]
+        public int EduSystemId { get; set; }
     }
 }
