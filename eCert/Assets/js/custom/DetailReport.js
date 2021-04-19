a409f51b2e23c7a04169f24b4d20356ee49f4360
BachHoang <bachbop9999@gmail.com>
2021-04-19 14:56:30 +0700

    finish report cert

---
+++ eCert/Assets/js/custom/DetailReport.js
@@ -0,0 +1,46 @@
+﻿$(document).ready(function () {
+    //getStatusByReportId(getUrlParameter('reportId'));
+});
+
+function getStatusByReportId(reportId) {
+    $.ajax({
+        type: "POST",
+        url: '/AcademicService/GetStatusByReportId',
+        context: document.body,
+        data: { reportId: reportId },
+        dataType: "json",
+        //contentType: 'application/json; charset=utf-8',
+        success: function (result) {
+            if (result == "Pending") {
+                $("#pending").prop("checked", true);
+            } else if (result == "Updated") {
+                $("#updated").prop("checked", true);
+            } else if (result == "Rejected") {
+                $("#rejected").prop("checked", true);
+            }
+        },
+        error: function (req, err) {
+            //debugger;  
+            console.log(err);
+            alert("Error has occurred..");
+        }
+    });
+}
+function sendForm() {
+    $("#updateStatus").submit();
+}
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
--- eCert/Models/Entity/Report.cs
+++ eCert/Models/Entity/Report.cs
@@ -7,6 +7,8 @@ namespace eCert.Models.Entity
 {
     public class Report
     {
+        public int ReportId { get; set; }
+        public string CertificateName { get; set; }
         public string Title { get; set; }
         public string ReportContent { get; set; }
         public string Status { get; set; }
@@ -15,5 +17,8 @@ namespace eCert.Models.Entity
         public DateTime CreateTime { get; set; }
 
         public DateTime UpdateTime { get; set; }
+        public string Fullname { get; set; }
+        public string Rollnumber { get; set; }
+
     }
 }
